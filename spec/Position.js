16f3b47409c4cfb1a6557ac59489a274675b8c45
Tibo <ttakado@thoughtworks.com>
2014-02-14 23:03:25 +0200

    Only valid directions are accepted & corrected previous tests

--- spec/Position.js
+++ spec/Position.js
@@ -1,8 +1,14 @@
 describe("Position", function(){
 	it("has x,y coordinates & a direction", function(){
-		var position = new Position(0, 1, "N")
-		expect(position.getXCoordinate()).toBe(0)
-		expect(position.getYCoordinate()).toBe(1)
-		expect(position.getDirection()).toBe("N")
+		var position = new Position(0, 1, Directions.N)
+		expect(position.getXCoordinate()).toEqual(0)
+		expect(position.getYCoordinate()).toEqual(1)
+		expect(position.getDirection()).toEqual(Directions.N)
+	})
+	it("accepts only valid directions", function(){
+		var incorrectDirection = function(){
+			var position = new Position(0, 1, "F")
+		}
+		expect(incorrectDirection).toThrow()
 	})
 });
--- src/Position.js
+++ src/Position.js
@@ -1,15 +1,29 @@
+var Directions = {
+	N:"North", S:"South", E:"East", W:"West",
+	isValid: function(direction){
+		return 	direction === "North" || direction === "South" || direction === "East" || direction === "West"
+	}
+}
+Object.freeze(Directions)
+
 function Position(xCoordinate, yCoordinate, direction){
 	var myXCoordinate = xCoordinate
 	var myYCoordinate = yCoordinate
+	if(!Directions.isValid(direction)){
+		throw{
+			name: "Incorrect Direction",
+			message: "An incorrect direction was provided"
+		}
+	}
 	var myDirection = direction
 
-	this.getXCoordinate= function(){
+	this.getXCoordinate = function(){
 		return myXCoordinate
 	}
-	this.getYCoordinate= function(){
+	this.getYCoordinate = function(){
 		return myYCoordinate
 	}
-	this.getDirection= function(){
+	this.getDirection = function(){
 		return myDirection
 	}
 }
