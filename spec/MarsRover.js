0a3afcead5e48299d98e6ba55c27fc1c8b2e6691
Tibo <ttakado@thoughtworks.com>
2014-02-15 14:54:47 +0200

    Refactored the matcher in tests

--- spec/MarsRover.js
+++ spec/MarsRover.js
@@ -1,18 +1,41 @@
 describe("Mars rover", function(){
+	var customMatchers = {
+		toHavePosition: function(util, customEqualityTesters) {
+			return{
+				compare: function(actual, expected) {
+					var result = {}
+					var actualPosition = actual.getPosition()
+					result.pass = util.equals(actualPosition.getXCoordinate(), expected.getXCoordinate()) 
+						&& util.equals(actualPosition.getYCoordinate(), expected.getYCoordinate())
+						&& util.equals(actualPosition.getCardinal(), expected.getCardinal())
+
+					if(result.pass){
+						result.message = "The Mars Rover has the correct position"
+					} else {
+						result.message = "Expected the Mars Rover to be at " 
+							+ expected.getXCoordinate() + " on x-axis, "
+							+ expected.getYCoordinate() + " on y-axis "
+							+ "and facing " + expected.getCardinal()
+					}
+					return result
+				}
+			}
+		}
+	}
+	beforeEach(function() {
+	    jasmine.addMatchers(customMatchers);
+	  })
+
 	it("can move forward when facing North", function(){
 		var initialPosition = new Position(0, 0, Cardinals.N)
 		var marsRover = new MarsRover(initialPosition)
 		marsRover.move("M")
-		expect(marsRover.getPosition().getXCoordinate()).toEqual(0)
-		expect(marsRover.getPosition().getYCoordinate()).toEqual(1)
-		expect(marsRover.getPosition().getCardinal()).toEqual(Cardinals.N)
+		expect(marsRover).toHavePosition(new Position(0, 1, Cardinals.N))
 	})
 	it("can move forward when facing South", function(){
 		var initialPosition = new Position(0, 1, Cardinals.S)
 		var marsRover = new MarsRover(initialPosition)
 		marsRover.move("M")
-		expect(marsRover.getPosition().getXCoordinate()).toEqual(0)
-		expect(marsRover.getPosition().getYCoordinate()).toEqual(0)
-		expect(marsRover.getPosition().getCardinal()).toEqual(Cardinals.S)
+		expect(marsRover).toHavePosition(new Position(0, 0, Cardinals.S))
 	})
 })
