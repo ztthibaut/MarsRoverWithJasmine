8e2dc24bab8abfbfdf4c7aaeb3c38dce2c23e1cb
Tibo <ttakado@thoughtworks.com>
2014-02-16 07:24:17 +0200

    Throw error if negative dimension is provided for a plateau

--- spec/Plateau.js
+++ spec/Plateau.js
@@ -9,4 +9,22 @@ describe("Plateau", function(){
 		expect(plateau.getXDimension()).toEqual(1)
 		expect(plateau.getYDimension()).toEqual(2)
 	})
+	it("throws error when negative x-dimension is provided", function(){
+		var giveNegativeXDimension = function(){
+			new Plateau(-1, 5)
+		}
+		expect(giveNegativeXDimension).toThrow({
+			name: "Incorrect dimension provide for plateau",
+			message: "The x-dimension -1 is not a valid plateau dimension"
+		})
+	})
+	it("throws error when negative y-dimension is provided", function(){
+		var giveNegativeXDimension = function(){
+			new Plateau(1, -5)
+		}
+		expect(giveNegativeXDimension).toThrow({
+			name: "Incorrect dimension provide for plateau",
+			message: "The y-dimension -5 is not a valid plateau dimension"
+		})
+	})
 })
--- src/Plateau.js
+++ src/Plateau.js
@@ -1,5 +1,17 @@
 function Plateau(xDimension, yDimension){
+	if(xDimension < 0){
+		throw{
+			name: "Incorrect dimension provide for plateau",
+			message: "The x-dimension " + xDimension + " is not a valid plateau dimension"
+		}
+	}
 	var myXDimension = xDimension
+	if(yDimension < 0){
+		throw{
+			name: "Incorrect dimension provide for plateau",
+			message: "The y-dimension " + yDimension + " is not a valid plateau dimension"
+		}
+	}
 	var myYDimension = yDimension
 	this.getXDimension =  function(){
 		return myXDimension
